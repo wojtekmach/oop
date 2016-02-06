18eb9842364eb4498aa9443c1f2c15171d22429c
Wojtek Mach <wojtek@wojtekmach.pl>
2016-02-06 14:08:02 +0100

    Instantiate empty object

--- lib/oop.ex
+++ lib/oop.ex
@@ -1,5 +1,13 @@
 defmodule OOP do
   def class(name, _body) do
+    contents =
+      quote do
+        def new do
+          %{class: unquote(name)}
+        end
+      end
+
+    Module.create(name, contents, Macro.Env.location(__ENV__))
     name
   end
 end
--- test/oop_test.exs
+++ test/oop_test.exs
@@ -3,10 +3,18 @@ defmodule OOPTest do
 
   test "define empty class" do
     import OOP
-
-    c = class Person do
+    c = class Person1 do
     end
 
-    assert c == Person
+    assert c == Person1
+  end
+
+  test "instantiate empty object" do
+    import OOP
+    class Person2 do
+    end
+
+    alice = Person2.new
+    assert alice.class == Person2
   end
 end
