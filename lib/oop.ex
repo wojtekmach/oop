0a383076e4f8f44f137e8271e9620b1429ac9ece
Wojtek Mach <wojtek@wojtekmach.pl>
2016-02-06 21:26:09 +0100

    Re-arrange code

--- lib/oop.ex
+++ lib/oop.ex
@@ -1,18 +1,4 @@
 defmodule OOP do
-  defmacro var(field) do
-    quote do
-      @fields unquote(field)
-
-      def unquote(field)() do
-        Agent.get(__MODULE__, fn data -> Map.get(data, unquote(field)) end)
-      end
-
-      def unquote(:"set_#{field}")(value) do
-        Agent.update(__MODULE__, fn data -> Map.update!(data, unquote(field), fn _ -> value end) end)
-      end
-    end
-  end
-
   defmacro class(name, contents) do
     quote do
       defmodule unquote(name) do
@@ -45,6 +31,20 @@ defmodule OOP do
           module.__init__(fields)
           module
         end
+      end
+    end
+  end
+
+  defmacro var(field) do
+    quote do
+      @fields unquote(field)
+
+      def unquote(field)() do
+        Agent.get(__MODULE__, fn data -> Map.get(data, unquote(field)) end)
+      end
+
+      def unquote(:"set_#{field}")(value) do
+        Agent.update(__MODULE__, fn data -> Map.update!(data, unquote(field), fn _ -> value end) end)
       end
     end
   end
