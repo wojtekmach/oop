71680a37744c535a0ba14c0a1018be92ea0aa072
Wojtek Mach <wojtek@wojtekmach.pl>
2016-05-06 19:54:09 +0200

    Define methods on objects

--- test/oop_test.exs
+++ test/oop_test.exs
@@ -19,6 +19,28 @@ defmodule OOPTest do
     purge Person
   end
 
+  test "define methods on objects" do
+    class Person do
+      def zero do
+        0
+      end
+
+      def sum1(a) do
+        a
+      end
+
+      def sum2(a, b) do
+        a + b
+      end
+    end
+
+    alice = Person.new
+    assert alice.zero() == 0
+    assert alice.sum1(1) == 1
+    assert alice.sum2(1, 2) == 3
+    purge Person
+  end
+
   defp purge(module) when is_atom(module) do
     :code.delete(module)
     :code.purge(module)
